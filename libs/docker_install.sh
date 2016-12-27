e62e05726dd6ee8404f3d34337c85d43812621e4
okram999 <nirish.okram@gmail.com>
2016-12-27 10:25:17 -0600

    adds the steps to install docker

--- libs/docker_install.sh
+++ libs/docker_install.sh
@@ -4,7 +4,18 @@ result=$?
 
 if [ $result -ne 0 ]; then 
    echo "docker not installed"
-   exit 1
+   #exit 1
+   
+   echo "installing docker on CentOS" 
+   curl -fsSL https://get.docker.com/ | sh
+   echo "enabling the docker service"
+   sudo systemctl enable docker.service
+   echo "starting the docker service"
+   sudo systemctl start docker
+    echo "testing the docker service"
+   sudo docker run --rm hello-world
+   
 else 
    echo "docker installed"
 fi
+
